70d12a9546b030487a243c33060c89abdb178472
baptiste dauphin <bap_dauphin@hotmail.fr>
2022-10-12 20:24:54 +0200

    new version

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'pages#photos'
+  get "about", to: "pages#about"
+  get "photos", to: "pages#photos"
   # Defines the root path route ("/")
   # root "articles#index"
 end
